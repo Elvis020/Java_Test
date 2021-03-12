2c6847d68a551c4006496bf88029ba59ef588728
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2021-03-12 17:33:17 +0000

    Lets see

---
+++ src/main/java/com/example/demoConnection/DemoConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoConnection/DemoConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
